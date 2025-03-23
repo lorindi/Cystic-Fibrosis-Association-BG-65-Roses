d256932bcb2aad293f322216f85baa3bacb84ea8
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-03-23 13:58:54 +0200

    Update Card.tsx

--- cf-turborepo/apps/next-app/src/app/components/cards/Card.tsx
+++ cf-turborepo/apps/next-app/src/app/components/cards/Card.tsx
@@ -17,7 +17,7 @@ function Card({
   const ArrowRightIcon = () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-5 w-5"
+      className="h-4 md:h-5 w-4 md:w-5"
       viewBox="0 0 20 20"
       fill="currentColor"
     >
@@ -30,7 +30,7 @@ function Card({
   );
   const truncateTitle = (
     text: string,
-    maxLength: number = 28
+    maxLength: number = 18
   ): string => {
     if (text.length <= maxLength) return text;
     return text.slice(0, maxLength) + "";
@@ -38,15 +38,16 @@ function Card({
 
   const truncateDescription = (
     text: string,
-    maxLength: number = 220
+    maxLength: number = 85
   ): string => {
     if (text.length <= maxLength) return text;
     return text.slice(0, maxLength) + "";
   };
   return (
-    <div className="flex flex-col relative rounded-[10px] shadow-lg overflow-hidden
+    <div className="flex flex-col relative rounded-[10px] shadow-lg overflow-hidden gap-2 sm:gap-4
     p-2 lg:p-4
     w-[176px] h-[224px]
+    sm:w-[255px] sm:h-[328px]
     md:w-[383px] md:h-[492px]
     lg:w-[430px] lg:h-[522px]
     ">
@@ -68,14 +69,12 @@ function Card({
           className="w-full h-full bg-cyan-50 bg-opacity-20 rounded-[10px]"
         />
       </div>
-      <div className="flex-1 flex flex-col gap-0 md:gap-2">
-        <ul className="flex gap-1 md:gap-2 w-full my-1 md:my-2 ">
+      <div className="flex-1 flex flex-col gap-2 sm:gap-4">
+        <ul className="hidden md:flex gap-1 md:gap-2 w-full">
           {tags.map((tag) => (
             <li
-              className=" rounded-[100px] outline-1 outline-[#737373] flex justify-center items-center text-[#737373]  font-['Montserrat']
-              text-[8px] px-1 py-0 font-bold
-              sm:text-[10px] 
-              md:text-xs md:px-[5px] md:py-[2px] md:font-medium
+              className=" rounded-[100px] outline-1 outline-[#737373] flex justify-center items-center text-[#737373] font-['Montserrat']
+              text-sm px-[5px] py-[2px] font-medium
               "
               key={tag}
             >
@@ -83,15 +82,17 @@ function Card({
             </li>
           ))}
         </ul>
-        <div className="flex flex-col md:gap-2 w-full h-full">
+        <div className="flex flex-col gap-0 sm:gap-2 w-full h-full overflow-hidden">
           <h2 className="text-color-heading font-semibold font-['Montserrat'] 
           text-sm 
           sm:text-base
           md:text-2xl
         ">{truncateTitle(title)}</h2>
-          <p className="text-color-paragraph font-normal font-['Montserrat'] text-xs leading-none
-          md:leading-5 md:text-base
-            lg:text-base lg:leading-normal
+          <p className="text-color-paragraph font-normal font-['Montserrat']
+            text-[11px]
+            sm:text-sm
+            md:text-base
+            lg:text-base 
         ">{truncateDescription(description)}</p>
         </div>
       </div>
