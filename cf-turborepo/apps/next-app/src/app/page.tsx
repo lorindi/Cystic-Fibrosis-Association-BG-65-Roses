0cd4e9ff44b5c075508558b38a21c828ded1d701
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-03-23 14:49:27 +0200

    Update page.tsx

--- cf-turborepo/apps/next-app/src/app/page.tsx
+++ cf-turborepo/apps/next-app/src/app/page.tsx
@@ -2,17 +2,28 @@
 import Image from "next/image";
 import Button from "./components/buttons/Button";
 import Card from "./components/cards/Card";
+import CardHistory from "./components/cards/CardHistory";
 export default function Home() {
   const handleClick = () => {
     alert("Button clicked!");
   };
   return (
-    <Card
-    imageUrl="https://images.unsplash.com/photo-1561525140-c2a4cc68e4bd?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    title="Cardafdhfgjhjjgkukyhkujjkvfjlh"
-    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. gggghfghfgQuisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur"
-    tags={["Tag 1", "Tag 2", "Tag 3"]}
-    onClick={handleClick}
-  />
+    <div className="flex">
+      <Card
+        link="https://www.google.com"
+        imageUrl="https://images.unsplash.com/photo-1561525140-c2a4cc68e4bd?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        title="Cardafdhfgjhjjgkukyhkujjkvfjlh"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. gggghfghfgQuisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur"
+        tags={["Tag 1", "Tag 2", "Tag 3"]}
+        onClick={handleClick}
+      />
+      <CardHistory
+        link="https://www.google.com"
+        imageUrl="https://images.unsplash.com/photo-1561525140-c2a4cc68e4bd?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        title="Cardafdhfgjhjjgkukyhkujjkvfjlh"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. gggghfghfgQuisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur"
+        onClick={handleClick}
+      />
+    </div>
   );
 }
