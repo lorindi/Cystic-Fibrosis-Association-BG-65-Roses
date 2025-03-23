227dfd23d8b95627ddc4427f02852e7ffec623cc
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-03-23 13:18:23 +0200

    Update Card.tsx

--- cf-turborepo/apps/next-app/src/app/components/cards/Card.tsx
+++ cf-turborepo/apps/next-app/src/app/components/cards/Card.tsx
@@ -28,25 +28,38 @@ function Card({
       />
     </svg>
   );
-  const truncateDescription = (
+  const truncateTitle = (
     text: string,
-    maxLength: number = 258
+    maxLength: number = 28
   ): string => {
     if (text.length <= maxLength) return text;
-    return text.slice(0, maxLength) + "...";
+    return text.slice(0, maxLength) + "";
+  };
+
+  const truncateDescription = (
+    text: string,
+    maxLength: number = 220
+  ): string => {
+    if (text.length <= maxLength) return text;
+    return text.slice(0, maxLength) + "";
   };
   return (
-    <div className="flex flex-col relative p-4 rounded-[10px] shadow-lg
+    <div className="flex flex-col relative rounded-[10px] shadow-lg overflow-hidden
+    p-2 lg:p-4
+    w-[176px] h-[224px]
+    md:w-[383px] md:h-[492px]
     lg:w-[430px] lg:h-[522px]
     ">
-      <div className="flex justify-end items-center absolute top-0 right-0">
+      <div className="absolute top-0 right-0 pb-2 pl-2 bg-white rounded-[10px]
+      lg:pb-4 lg:pl-4
+      ">
         <Button
           type="filled-icon"
           icon={<ArrowRightIcon />}
           onClick={onClick}
         />
       </div>
-      <div className="flex-1 flex justify-center items-center w-full h-full">
+      <div className="flex-1">
         <Image
           src={imageUrl}
           alt={title}
@@ -55,23 +68,30 @@ function Card({
           className="w-full h-full bg-cyan-50 bg-opacity-20 rounded-[10px]"
         />
       </div>
-      <div className="flex-1 w-full h-full my-2 flex flex-col gap-2">
-        <ul className="flex gap-2 ">
+      <div className="flex-1 flex flex-col gap-0 md:gap-2">
+        <ul className="flex gap-1 md:gap-2 w-full my-1 md:my-2 ">
           {tags.map((tag) => (
             <li
-              className="px-[5px] py-[2px] rounded-[100px] outline-1 outline-[#737373] flex justify-center items-center text-[#737373] text-xs font-medium font-['Montserrat']"
+              className=" rounded-[100px] outline-1 outline-[#737373] flex justify-center items-center text-[#737373]  font-['Montserrat']
+              text-[8px] px-1 py-0 font-bold
+              sm:text-[10px] 
+              md:text-xs md:px-[5px] md:py-[2px] md:font-medium
+              "
               key={tag}
             >
               {tag}
             </li>
           ))}
         </ul>
-        <div className="flex flex-col">
-          <h2 className="text-color-heading font-semibold font-['Montserrat']
-        lg:text-2xl
-        ">{title}</h2>
-          <p className="text-color-paragraph  font-normal font-['Montserrat'] leading-normal
-        lg:text-base
+        <div className="flex flex-col md:gap-2 w-full h-full">
+          <h2 className="text-color-heading font-semibold font-['Montserrat'] 
+          text-sm 
+          sm:text-base
+          md:text-2xl
+        ">{truncateTitle(title)}</h2>
+          <p className="text-color-paragraph font-normal font-['Montserrat'] text-xs leading-none
+          md:leading-5 md:text-base
+            lg:text-base lg:leading-normal
         ">{truncateDescription(description)}</p>
         </div>
       </div>
