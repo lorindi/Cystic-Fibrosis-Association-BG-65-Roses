b95b5fd6899d1eb9a66325bd4ed3a14b7d7a36ce
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-03-25 10:52:57 +0200

    Create Card frame

---
+++ cf-turborepo/apps/next-app/src/app/components/cards/CardFrame.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+import logoLung from "../../../../public/images/logoLung.png";
+function CardFrame({ imageUrls }: { imageUrls: string[] }) {
+  return (
+    <div className="grid grid-cols-2 gap-4 w-full h-full max-w-[582px] max-h-[632px] relative">
+      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 border-[12px]  w-25 h-25 sm:w-30 sm:h-30 md:w-35 md:h-35 lg:w-44 lg:h-44 bg-sky-100 rounded-full lg:border-[24px] border-white" />
+      <Image src={logoLung} alt="Logo" width={100} height={100} className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-18 h-18 p-2 sm:w-20 sm:h-20 sm:p-2 md:w-28 md:h-28 md:p-4 lg:w-30 lg:h-30 lg:p-4" />
+      {imageUrls.map((imageUrl) => (
+        <Image
+          src={imageUrl}
+          alt="Card Frame"
+          width={100}
+          height={100}
+          className="w-32 h-40 sm:w-52 sm:h-56 md:w-72 md:h-80 lg:w-72 lg:h-80 bg-cyan-50 bg-opacity-10 rounded-[10px] object-cover"
+        />
+      ))}
+    </div>
+  );
+}
+
+export default CardFrame;
