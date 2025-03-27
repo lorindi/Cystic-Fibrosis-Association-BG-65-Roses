8995567f5e36b0246f2b8d619b7fac6beb576f2c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-03-27 08:54:10 +0200

    Update AboutUsSection.tsx

--- cf-turborepo/apps/next-app/src/app/home/components/AboutUsSection.tsx
+++ cf-turborepo/apps/next-app/src/app/home/components/AboutUsSection.tsx
@@ -3,12 +3,44 @@ import React from "react";
 
 function AboutUsSection() {
   return (
-    <div className="flex flex-col items-center justify-center h-auto my-24 px-4 gap-18">
-        <CardFrame imageUrls={['/images/rectangle1.jpeg', '/images/rectangle2.jpeg','/images/rectangle3.jpeg','/images/rectangle4.jpeg',]} />
-      <div className="flex flex-col justify-center items-center gap-4">
-        <span className="self-stretch text-center justify-start text-sky-800 text-xs font-medium font-['Montserrat']">WHO ARE WE?</span>
-        <h4 className="self-stretch text-center justify-start text-color-heading text-base font-medium font-['Montserrat'] leading-normal">Information about upcoming conferences and events</h4>
-        <p className="self-stretch text-center justify-start text-color-heading text-xs font-normal font-['Montserrat'] leading-tight">
+    <div className="flex flex-col xl:flex-row items-center justify-center h-auto my-24 xl:my-[232px] xl:py-[152px] px-4 xl:px-16 gap-18 xl:gap-16">
+      <CardFrame
+        imageUrls={[
+          "/images/rectangle1.jpeg",
+          "/images/rectangle2.jpeg",
+          "/images/rectangle3.jpeg",
+          "/images/rectangle4.jpeg",
+        ]}
+      />
+      <div
+        className="flex flex-col justify-center items-center gap-4 px-4 xl:px-0 max-w-[271px]
+           sm:max-w-[430px]
+           md:max-w-[582px]"
+      >
+        <span
+          className="self-stretch text-center justify-start text-sky-800 text-xs font-medium font-['Montserrat']
+            sm:text-base sm:leading-normal
+            xl:text-left
+            "
+        >
+          WHO ARE WE?
+        </span>
+        <h4
+          className="self-stretch text-center justify-start text-color-heading text-base font-medium font-['Montserrat'] leading-normal
+        sm:text-xl sm:leading-loose
+        md:text-2xl md:leading-9
+        xl:text-left
+
+        "
+        >
+          Information about upcoming conferences and events
+        </h4>
+        <p
+          className="self-stretch text-center justify-start text-color-heading text-xs font-normal font-['Montserrat'] leading-tight
+        sm:text-base sm:leading-normal
+        xl:text-left
+        "
+        >
           The website is designed to inform, inspire, and support people
           affected by cystic fibrosis – both patients and their loved ones.
           Here, you will find the latest news and events, valuable information
