8a3e27c6e100afcaf6ace33207c82356b2cdf375
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-03-27 08:28:50 +0200

    Create AboutUsSection.tsx

---
+++ cf-turborepo/apps/next-app/src/app/home/components/AboutUsSection.tsx
@@ -0,0 +1,26 @@
+import CardFrame from "@/app/components/cards/CardFrame";
+import React from "react";
+
+function AboutUsSection() {
+  return (
+    <div className="flex flex-col items-center justify-center h-auto my-24 px-4 gap-18">
+        <CardFrame imageUrls={['/images/rectangle1.jpeg', '/images/rectangle2.jpeg','/images/rectangle3.jpeg','/images/rectangle4.jpeg',]} />
+      <div className="flex flex-col justify-center items-center gap-4">
+        <span className="self-stretch text-center justify-start text-sky-800 text-xs font-medium font-['Montserrat']">WHO ARE WE?</span>
+        <h4 className="self-stretch text-center justify-start text-color-heading text-base font-medium font-['Montserrat'] leading-normal">Information about upcoming conferences and events</h4>
+        <p className="self-stretch text-center justify-start text-color-heading text-xs font-normal font-['Montserrat'] leading-tight">
+          The website is designed to inform, inspire, and support people
+          affected by cystic fibrosis – both patients and their loved ones.
+          Here, you will find the latest news and events, valuable information
+          about the disease, inspiring stories, and an online store with
+          handmade products. The proceeds from sales and donations support the
+          cause of improving patients' lives. The project is carried out by a
+          team of IT specialists united by the desire to make a positive impact
+          and support the community in the fight against cystic fibrosis.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUsSection;
