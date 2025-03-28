169f539c9ec4d06ebc4467c2b3c179b04117f0f6
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-03-28 17:15:44 +0200

    Update page.tsx

--- cf-turborepo/apps/next-app/src/app/page.tsx
+++ cf-turborepo/apps/next-app/src/app/page.tsx
@@ -4,18 +4,19 @@ import HeroSection from "./home/components/HeroSection";
 import NavigationCards from "./home/components/NavigationCards";
 import ContributeSection from "./home/components/ContributeSection";
 import NewsSection from "./home/components/NewsSection";
-
+import PatientStoriesSection from "./home/components/PatientStoriesSection";
 export default function Home() {
   const handleClick = () => {
     alert("Button clicked!");
   };
   return (
-    <div className="w-full h-full">
+    <div className="flex flex-col justify-center items-center w-full h-full">
       <HeroSection />
       <NavigationCards />
       <AboutUsSection />
       <ContributeSection />
       <NewsSection />
+      <PatientStoriesSection />
     </div>
   );
 }
