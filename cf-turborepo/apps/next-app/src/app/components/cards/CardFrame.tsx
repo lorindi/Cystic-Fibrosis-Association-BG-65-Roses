1ce99c6c17b2f28069cdff4a7db49632327842e5
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-03-28 14:54:00 +0200

    Update CardFrame.tsx

--- cf-turborepo/apps/next-app/src/app/components/cards/CardFrame.tsx
+++ cf-turborepo/apps/next-app/src/app/components/cards/CardFrame.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import logoLung from "../../../../public/images/logoLung.png";
 function CardFrame({ imageUrls }: { imageUrls: string[] }) {
   return (
-    <div className="grid grid-cols-2 gap-4 w-full h-full max-w-[271px] max-h-[346px] sm:max-w-[430px] sm:max-h-[468px] md:max-w-[582px] md:max-h-[632px] relative">
+    <div className="grid grid-cols-2 gap-4 w-full h-full max-w-[271px] max-h-[346px] sm:max-w-[430px] sm:max-h-[468px] md:max-w-[582px] md:max-h-[662px] relative">
       <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 border-[12px]  w-25 h-25 sm:w-30 sm:h-30 md:w-35 md:h-35 lg:w-44 lg:h-44 bg-sky-100 rounded-full lg:border-[24px] border-white" />
       <Image
         src={logoLung}
