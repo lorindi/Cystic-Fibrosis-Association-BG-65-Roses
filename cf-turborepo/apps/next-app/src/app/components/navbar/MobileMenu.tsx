e4bf64e09587221827efa92b242dfecf7426465d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-03-31 20:42:16 +0300

    Update MobileMenu.tsx

--- cf-turborepo/apps/next-app/src/app/components/navbar/MobileMenu.tsx
+++ cf-turborepo/apps/next-app/src/app/components/navbar/MobileMenu.tsx
@@ -2,16 +2,14 @@
 import { useState } from "react";
 import Link from "next/link";
 import Logo from "../logo/Logo";
-import { usePathname } from "next/navigation";
 
 function MobileMenu() {
   const [isOpen, setIsOpen] = useState(false);
-  const pathname = usePathname();
-  const isAdmin = pathname.startsWith('/admin');
+
 
 
   return (
-    <div className={`${isAdmin ? 'hidden' : ''}`}>
+    <div>
       <div className="flex items-center justify-between">
         <Logo type="navigation" />
 
