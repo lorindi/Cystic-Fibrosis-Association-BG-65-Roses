d254fd5d6876b79716d7570f6450dc54580f873e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-03-31 20:42:21 +0300

    Update Navbar.tsx

--- cf-turborepo/apps/next-app/src/app/components/navbar/Navbar.tsx
+++ cf-turborepo/apps/next-app/src/app/components/navbar/Navbar.tsx
@@ -1,22 +1,26 @@
-"use client"
-import React from 'react'
-import WebMenu from './WebMenu'
-import MobileMenu from './MobileMenu'
-
+"use client";
+import React from "react";
+import WebMenu from "./WebMenu";
+import MobileMenu from "./MobileMenu";
+import { usePathname } from "next/navigation";
 
 function Navbar() {
+  const pathname = usePathname();
+  const isAdmin =
+    pathname.startsWith("/admin") ||
+    pathname.startsWith("/sign-in") ||
+    pathname.startsWith("/create-account");
   return (
-    <div className='w-full p-4'>
-      <div className='hidden lg:flex w-full'>
-        <WebMenu/>
+    <div className={`${isAdmin ? "hidden" : ""} w-full p-4`}>
+      <div className="hidden lg:flex w-full">
+        <WebMenu />
       </div>
 
-      <div className='lg:hidden w-full'>
-        <MobileMenu/>
+      <div className="lg:hidden w-full">
+        <MobileMenu />
       </div>
-    
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
