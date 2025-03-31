da4cbf7bfdcc83c18855459526470aba75a8be4f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-03-31 16:45:32 +0300

    Create ApolloProvider.tsx

---
+++ cf-turborepo/apps/next-app/src/components/providers/ApolloProvider.tsx
@@ -0,0 +1,10 @@
+import { ApolloProvider as BaseApolloProvider } from '@apollo/client';
+import { client } from '@/lib/apollo-client';
+
+export function ApolloProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <BaseApolloProvider client={client}>
+      {children}
+    </BaseApolloProvider>
+  );
+} 
