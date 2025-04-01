44ac849a11a87b0f1dcc713c1fd91a8a2e26100a
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-04-01 16:09:37 +0300

    Create mutations.ts

---
+++ cf-turborepo/apps/next-app/src/lib/apollo/mutations.ts
@@ -0,0 +1,91 @@
+import { gql } from '@apollo/client';
+import { AuthResponse, RegisterInput, LoginInput, VerificationResponse } from './types';
+
+export const REGISTER = gql`
+  mutation Register($input: RegisterInput!) {
+    register(input: $input) {
+      token
+      user {
+        _id
+        name
+        email
+        role
+        isEmailVerified
+      }
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation Login($input: LoginInput!) {
+    login(input: $input) {
+      token
+      user {
+        _id
+        name
+        email
+        role
+        isEmailVerified
+      }
+    }
+  }
+`;
+
+export const VERIFY_EMAIL = gql`
+  mutation VerifyEmail($token: String!) {
+    verifyEmail(token: $token) {
+      success
+      message
+      user {
+        _id
+        name
+        email
+        role
+        isEmailVerified
+      }
+      token
+    }
+  }
+`;
+
+export const RESEND_VERIFICATION_EMAIL = gql`
+  mutation ResendVerificationEmail {
+    resendVerificationEmail
+  }
+`;
+
+export const GET_CURRENT_USER = gql`
+  query GetCurrentUser {
+    getCurrentUser {
+      _id
+      name
+      email
+      role
+      groups
+      isEmailVerified
+      profile {
+        avatar
+        bio
+        birthDate
+        address {
+          city
+          postalCode
+          street
+        }
+        contact {
+          phone
+          alternativeEmail
+          emergencyContact {
+            name
+            phone
+            relation
+          }
+        }
+        diagnosed
+        diagnosisYear
+        childName
+        companyName
+      }
+    }
+  }
+`; 
