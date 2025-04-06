f58aef8e239e845161f9284f857258a02c67a8db
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-04-06 18:08:49 +0300

    Create index.ts

---
+++ cf-turborepo/apps/next-app/src/app/admin/features/campaigns/components/index.ts
@@ -0,0 +1,6 @@
+export * from "./CampaignsTable";
+export * from "./CampaignFormModal";
+export * from "./CampaignEventsTable";
+export * from "./CampaignEventFormModal";
+export * from "./PendingParticipantsTable";
+export * from "./CampaignParticipantsDialog"; 
